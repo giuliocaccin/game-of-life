efd8bf6bf88819eeee15699104b2f4f55cd72685
Giulio Caccin <giulio.caccin@scania.com>
2021-03-28 00:39:42 +0100

    shugar

--- src/main/kotlin/Domain.kt
+++ src/main/kotlin/Domain.kt
@@ -9,44 +9,46 @@ sealed class Cell {
 class World private constructor(private val matrix: List<List<Block>>) {
     fun life(): World {
         return World(matrix.map { row ->
-            row.map { block ->
-                val current = block.cell
-                val aliveNeighbours = block.neighborArea
+            row.map { current ->
+                val aliveNeighbor = current.neighborArea
                     .map { position -> matrix[position.x][position.y] }
                     .map { neighbour -> neighbour.cell }
                     .count { cell -> cell === Alive }
-                if (current === Alive && aliveNeighbours < 2)
-                    block.copy(cell = Dead)
-                else if (current === Alive && (aliveNeighbours == 2 || aliveNeighbours == 3))
-                    block.copy(cell = Alive)
-                else if (current === Alive && aliveNeighbours > 3)
-                    block.copy(cell = Dead)
-                else if (current === Dead && aliveNeighbours == 3)
-                    block.copy(cell = Alive)
-                else if(current === Dead)
-                    block.copy(cell = Dead)
-                else
-                    throw IllegalStateException()
+
+                when (current.cell) {
+                    is Alive -> {
+                        when {
+                            aliveNeighbor < 2 -> current.copy(cell = Dead)
+                            aliveNeighbor > 3 -> current.copy(cell = Dead)
+                            else -> current
+                        }
+                    }
+                    is Dead -> {
+                        when (aliveNeighbor) {
+                            3 -> current.copy(cell = Alive)
+                            else -> current
+                        }
+                    }
+                }
             }
-        })
+        }
+        )
     }
 
     fun toBoard(): Array<IntArray> =
-        matrix
-            .map { row ->
-                row.map { block ->
-                    when (block.cell) {
-                        is Dead -> 0
-                        is Alive -> 1
-                    }
-                }.toIntArray()
-            }.toTypedArray()
+        matrix.map { row ->
+            row.map {
+                when (it.cell) {
+                    is Dead -> 0
+                    is Alive -> 1
+                }
+            }.toIntArray()
+        }.toTypedArray()
 
     companion object {
         fun fromBoard(board: Array<IntArray>): World =
             World(board.mapIndexed { x, row ->
-                row
-                    .mapIndexed { y, value -> (if (value == 1) Alive else Dead) to Position(x, y) }
+                row.mapIndexed { y, value -> (if (value == 1) Alive else Dead) to Position(x, y) }
                     .map { pair -> Block(pair.first, getNeighbourArea(pair.second, board)) }
             })
     }
@@ -57,11 +59,24 @@ fun getNeighbourArea(center: Position, board: Array<IntArray>): Set<Position> =
     (center.x - 1..center.x + 1).flatMap { x ->
         (center.y - 1..center.y + 1).map { y -> Position(x, y) }
     }
-        .asSequence()
-        .map { position -> if (position.x < 0) position.copy(x = board.size - 1) else position }
-        .map { position -> if (position.x >= board.size) position.copy(x = 0) else position }
-        .map { position -> if (position.y < 0) position.copy(y = board[position.x].size - 1) else position }
-        .map { position -> if (position.y >= board[position.x].size) position.copy(y = 0) else position }
+        .map { position ->
+            with(position) {
+                when {
+                    x < 0 -> copy(x = board.size - 1)
+                    x >= board.size -> copy(x = 0)
+                    else -> this
+                }
+            }
+        }
+        .map { position ->
+            with(position) {
+                when {
+                    y < 0 -> copy(y = board[position.x].size - 1)
+                    y >= board[position.x].size -> copy(y = 0)
+                    else -> this
+                }
+            }
+        }
         .filter { position -> position != center }
         .toSet()
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,16 @@
-import java.util.*
-import kotlin.math.pow
 import kotlin.random.Random
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 fun main(args: Array<String>) {
     val edgeSize = 100
-    val seed = generateSeed(edgeSize.toDouble().pow(2.0).toInt())
+    val rand = Random
     var board: Array<IntArray> = IntArray(edgeSize)
-        .map { IntArray(edgeSize) { seed.pop() } }
+        .map { IntArray(edgeSize) { rand.nextInt(0, 2) } }
         .toTypedArray()
 
-    println("Start")
+    println("Seed")
     renderBoard(board)
-    println()
 
     var tick = 1
     while (true) {
@@ -23,20 +20,13 @@ fun main(args: Array<String>) {
 
         println("Tick nr: ${tick++}")
         renderBoard(board)
-        println()
 
         Thread.sleep(500)
     }
 }
 
-private fun generateSeed(length: Int): Stack<Int> {
-    val rand = Random
-    val seed = Stack<Int>()
-    seed.addAll(IntArray(length) { rand.nextInt(0, 2) }.toTypedArray())
-    return seed
-}
-
 private fun renderBoard(board: Array<IntArray>) {
     board.map { line -> println(line.joinToString("") { i -> if (i == 1) "█" else " " }) }
+    println()
 }
 
